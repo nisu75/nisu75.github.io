9f781e20cb9b7eaeb3eb18ddb33ba1f3f1c36ff4
Nicole Cui <nicolexcui@gmail.com>
2024-01-12 21:43:09 -0500

    POGGING CAT (fixed navbar)

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,23 @@
+import React, { useState } from "react";
 import "./Navbar.css";
 
 export default function NavDisplay() {
+    const notClicked = <a href = "#home">∧,,,,∧<br/>( ｡･ω･)</a>;
+    const clicked = <a href = "#home">∧,,,,∧<br/>( ｡･o･)</a>;
+
+    const [homeButton, setHomeButton] = React.useState(notClicked);
+
+    const mouseDown = () => {
+        setHomeButton(clicked);
+    };
+
+    const mouseUp = () => {
+        setHomeButton(notClicked);
+    };
+
     return (<div className = "navbar">
         <ul className = "topbar">
-            <li className = "homebutton"><a href = "#home">∧,,,,∧<br></br>( ｡･ω･)</a></li>
+            <li className = "homebutton" onMouseDown={mouseDown} onMouseUp={mouseUp}>{homeButton}</li>
             <li className = "navbutton"><a href = "#projects">Projects</a></li>
             <li className = "navbutton"><a href = "#fun">Fun</a></li>
             <li className = "navbutton"><a href = "#about">About</a></li>       

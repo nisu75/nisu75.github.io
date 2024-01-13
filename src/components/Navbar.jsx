cbe971868e2a10ada0564b52c4642d94e9acd2ed
Nicole Cui <nicolexcui@gmail.com>
2024-01-13 18:12:08 -0500

    navbar to navlinks, added active formatting

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
 import "./Navbar.css";
 
 export default function NavDisplay() {
+    /*
     const pogCat = <a href = "/">&nbsp;∧,,,,∧<br/>( ｡･o･)</a>;
     const cat0 = <a href = "/">&nbsp;∧,,,,∧<br/>( ｡･ω･)</a>;
     const cat1 = <a href = "/">&nbsp;∧,,,,∧<br/>( „• ᴗ •„)</a>;
@@ -9,6 +11,14 @@ export default function NavDisplay() {
     const cat3 = <a href = "/">&nbsp;∧,,,,∧<br/>(  *ˊ▽ˋ*)</a>;
     const cat4 = <a href = "/">&nbsp;∧,,,,∧<br/>(  ◕‿◕)</a>;
     const cat5 = <a href = "/">&nbsp;∧,,,,∧<br/>( ｡ -ω-)</a>;
+    */
+    const pogCat = <text>&nbsp;∧,,,,∧<br/>( ｡･o･)</text>;
+    const cat0 = <text>&nbsp;∧,,,,∧<br/>( ｡･ω･)</text>;
+    const cat1 = <text>&nbsp;∧,,,,∧<br/>( „• ᴗ •„)</text>;
+    const cat2 = <text>&nbsp;∧,,,,∧<br/>( ✧ω✧)</text>;
+    const cat3 = <text>&nbsp;∧,,,,∧<br/>(  *ˊ▽ˋ*)</text>;
+    const cat4 = <text>&nbsp;∧,,,,∧<br/>(  ◕‿◕)</text>;
+    const cat5 = <text>&nbsp;∧,,,,∧<br/>( ｡ -ω-)</text>;
 
     const catArray = [cat0, cat1, cat2, cat3, cat4, cat5, pogCat];
 
@@ -34,16 +44,22 @@ export default function NavDisplay() {
         while (rand == currCat);
         setCurrCat(rand);
         setCat(catArray[currCat]);
-
-        //console.log(`rand: ${rand}, currCat: ${currCat}`);
     }
 
     return (<div className = "navbar">
         <ul className = "topbar">
-            <li className = "homebutton" onMouseDown={mouseDown} onMouseUp={mouseUp} onMouseEnter={mouseHover}>{cat}</li>
-            <li className = "navbutton"><a className = "navlink" href = "/#/projects">Projects</a></li>
-            <li className = "navbutton"><a className = "navlink" href = "/#/fun">Fun</a></li>
-            <li className = "navbutton"><a className = "navlink" href = "/#/about">About</a></li>       
+            <li className = "homebutton" onMouseDown={mouseDown} onMouseUp={mouseUp} onMouseEnter={mouseHover}>
+                <NavLink exact to="/">{cat}</NavLink>
+            </li>
+            <li className = "navbutton">
+                <NavLink className = "navlink" to="/projects">Projects</NavLink>
+            </li>
+            <li className = "navbutton">
+                <NavLink className = "navlink" to="/fun">Fun</NavLink>
+            </li>
+            <li className = "navbutton">
+                <NavLink className = "navlink" to="/about">About</NavLink>
+            </li>       
         </ul>
     </div>)
 }

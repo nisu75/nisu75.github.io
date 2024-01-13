fa55a581175a71e704e0af81791c229b0e4a8334
Nicole Cui <nicolexcui@gmail.com>
2024-01-12 19:59:32 -0500

    added navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,12 @@
-import "./Home.css";
+import "./Navbar.css";
 
 export default function NavDisplay() {
-
+    return (<div className = "navbar">
+        <ul className = "topbar">
+            <li className = "homebutton"><a href = "#home">∧,,,,∧<br></br>( ｡･ω･)</a></li>
+            <li className = "navbutton"><a href = "#projects">Projects</a></li>
+            <li className = "navbutton"><a href = "#fun">Fun</a></li>
+            <li className = "navbutton"><a href = "#about">About</a></li>       
+        </ul>
+    </div>)
 }

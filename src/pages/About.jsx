a3f0863879365560d30638bf49c78dd705cf9a50
Nicole Cui <nicolexcui@gmail.com>
2024-01-19 01:57:52 -0500

    updating about page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -5,13 +5,13 @@ export default function AboutDisplay() {
     return (<div className="page">
         <div className="text">
             <div className="abtdescription">
-            Nice to meet you! I'm Nicole, a Computer Science student at the University of Waterloo. 
+            Nice to meet you! I'm Nicole Cui, a Computer Science student at the University of Waterloo. 
             <br /><br />
-            I'm interested in cybersecurity and front-end development, as well as digital product design. I've dabbled in photo and video editing, and done more than just dabbling in graphic design, music, and visual art. 
+            I'm interested in cybersecurity and front-end development, as well as digital product design. I've dabbled in photo and video editing through Adobe Suite, and done more than just dabbling in graphic design, music, and visual art. 
             <br /><br />
             I enjoy solving problems and facing new challenges, perfect for navigating the constantly evolving tech landscape. I appreciate the constant exploration and learning that new projects bring, and I'd love to pursue technology that drives significant and impactful real-world advancements.
             <br /><br />
-            Feel free to reach out - I'd love to chat and connect more!
+            Feel free to reach out - I'd love to chat and connect more! (*´▽`)ﾉ
             </div>
 
             {/*
